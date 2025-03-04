15af24f4b2fba9f9b6662792d5aa772658b6fb91
Ishmeet Tuteja <ishmeet.tuteja@gmail.com>
2025-03-04 00:54:24 -0500

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      forward = session[:forwarding_url]
       reset_session
       log_in user
-      redirect_to user
+      redirect_to forward || user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new', status: :unprocessable_entity
