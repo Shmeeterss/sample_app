a347f9d40496b7bcc8759dcee508744d979c8f8c
Ishmeet Tuteja <ishmeet.tuteja@gmail.com>
2025-03-04 19:37:55 -0500

    Getting the correct link put in for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'http://enigmatic-ravine-97022.herokuapp.com'
+  host = 'https://enigmatic-ravine-97022-53772a907828.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
